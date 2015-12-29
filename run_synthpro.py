51d8576a5b5640d7cf426a05fb95aa8a391daaf2
cdr30 <roberts.cd@gmail.com>
2015-12-29 22:45:06 +0000

    Added top level script for executing synthpro from command line.

---
+++ run_synthpro.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+
+"""
+Top level script used to execute SynthPro package from command line.
+
+"""
+
+
+import sys
+
+from synthpro.synthpro import main
+
+if __name__ == '__main__':
+    
+    try:
+        main()
+    except KeyboardInterrupt as err:
+        print err
+        sys.exit()
+        
+        
